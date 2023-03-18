49a20b8b6e62ddd6b09794671a694908768b1c49
Jørgen Reimers <jorgenreim@gmail.com>
2023-03-18 15:13:02 +0100

    Create implementation classes of ScoreAPI
    Android implmementation initializes a Firebase database instance, rest
    is TODO
    
    Desktop implementation prints not implmemented error message

--- desktop/src/tdt4240/group2/DesktopLauncher.java
+++ desktop/src/tdt4240/group2/DesktopLauncher.java
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Sumo Battle Tap");
-		new Lwjgl3Application(new SumoBattleTapGame(), config);
+		new Lwjgl3Application(new SumoBattleTapGame(new DesktopScoreAPI()), config);
 	}
 }
