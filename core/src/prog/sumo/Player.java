789dff411f65e30e317d4f9e2cc21dd06d47d47d
Mariam Mardanghom <mariamm@stud.ntnu.no>
2023-04-02 22:27:13 +0200

    Added player logic and some player collusion logic for sprint 2

---
+++ core/src/prog/sumo/Player.java
@@ -0,0 +1,58 @@
+package prog.sumo;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Player {
+    private static final int MAX_POSITION = 4;
+    private int position;
+    private int direction;
+    private Texture texture;
+
+    public Player(Texture texture, int direction) {
+        this.texture = texture;
+        this.direction = direction;
+        if (direction == 1) {
+            position = 0;
+        } else {
+            position = MAX_POSITION;
+        }
+    }
+
+    public void moveForward() {
+        if (direction == 1) {
+            position++;
+        } else {
+            position--;
+        }
+    }
+
+    public void moveBackward() {
+        if (direction == 1) {
+            position--;
+        } else {
+            position++;
+        }
+    }
+
+    public void movePlayer(Player otherPlayer) {
+        if (Math.abs(otherPlayer.getPosition() - this.getPosition()) == 1){
+            this.moveForward();
+            otherPlayer.moveBackward();
+        }
+        else {
+            this.moveForward();
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+}
