1658b7196cb6a8f2b5d23140f41c7f758823ad95
Mariam Mardanghom <mariamm@stud.ntnu.no>
2023-04-14 14:08:25 +0200

    Selected two characters to make play appear

--- core/src/prog/sumo/states/PlayState.java
+++ core/src/prog/sumo/states/PlayState.java
@@ -9,9 +9,12 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import java.util.ArrayList;
 
 import prog.sumo.sprites.Player;
 
@@ -37,7 +40,7 @@ public class PlayState extends State {
 
     Stage stage;
 
-    public PlayState(GameStateManager gsm) {
+    public PlayState(GameStateManager gsm, ArrayList<Integer> selectedC) {
         super(gsm);
         shapeRenderer = new ShapeRenderer();
         settingsWheel = new Texture("settingswheel.png");
