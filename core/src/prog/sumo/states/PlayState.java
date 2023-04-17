c8fc10539f33343bc363639639363eb1c2d1ed55
annaostm <127393978+annaostm@users.noreply.github.com>
2023-04-17 15:58:38 +0200

    feat: create settings and tutorial (#26)

--- core/src/prog/sumo/states/PlayState.java
+++ core/src/prog/sumo/states/PlayState.java
@@ -1,13 +1,18 @@
 package prog.sumo.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -20,16 +25,19 @@ public class PlayState extends State {
     public static Texture char1, char2;
     public static int battleCircleHeight = Gdx.graphics.getHeight() / 2;
     public static int battleCircleRadius = Gdx.graphics.getWidth() / 2 + 20;
-    Texture settingsWheel;
-    Texture hand1Tex, hand2Tex;
-    ShapeRenderer shapeRenderer;
-    Drawable settingsWheelDrawable;
-    Drawable player1Drawable, player2Drawable;
-    ImageButton settingsB;
-    ImageButton hand1, hand2;
-    Stage stage;
     private final Player player1;
     private final Player player2;
+    Texture settingsWheel, windowTex, backTex, quitTex, tutorialTex, tutWinTex,
+            writtenTutTex, back2Tex;
+    Texture hand1Tex, hand2Tex;
+    ShapeRenderer shapeRenderer;
+    Drawable settingsWheelDrawable, windowDraw, backDraw, quitDraw,
+            tutorialDraw, tutWinDraw, writtenTutDraw, back2Draw;
+    Drawable player1Drawable, player2Drawable;
+    ImageButton settingsB, quitB, backB, tutB, writtenTutB, back2B;
+    ImageButton hand1, hand2;
+    Stage stage;
+    Window pinkWindow, orangeWindow;
 
     public PlayState(GameStateManager gsm, Map<Integer, String> playerHash) {
 
@@ -42,14 +50,43 @@ public class PlayState extends State {
         hand2Tex = new Texture("purplehand.png");
         char1 = new Texture(playerHash.get(0));
         char2 = new Texture(playerHash.get(1));
+        windowTex = new Texture("pinkWindow.png");
+        quitTex = new Texture("quitGame.png");
+        backTex = new Texture("backToGame.png");
+        tutorialTex = new Texture("tutorialOrangeButton.png");
+        tutWinTex = new Texture("orangeWindow.png");
+        writtenTutTex = new Texture("writtenTutorial.png");
+        back2Tex = new Texture("back2.png");
 
         settingsWheelDrawable = new TextureRegionDrawable(settingsWheel);
         player1Drawable = new TextureRegionDrawable(hand1Tex);
         player2Drawable = new TextureRegionDrawable(hand2Tex);
+        windowDraw = new TextureRegionDrawable(windowTex);
+        quitDraw = new TextureRegionDrawable(quitTex);
+        backDraw = new TextureRegionDrawable(backTex);
+        tutorialDraw = new TextureRegionDrawable(tutorialTex);
+        tutWinDraw = new TextureRegionDrawable(tutWinTex);
+        writtenTutDraw = new TextureRegionDrawable(writtenTutTex);
+        back2Draw = new TextureRegionDrawable(back2Tex);
 
         settingsB = new ImageButton(settingsWheelDrawable);
+        quitB = new ImageButton(quitDraw);
+        quitB.setTransform(true);
+        quitB.setScale(2f);
+        backB = new ImageButton(backDraw);
+        backB.setTransform(true);
+        backB.setScale(2f);
+        tutB = new ImageButton(tutorialDraw);
+        tutB.setTransform(true);
+        tutB.setScale(2f);
         hand1 = new ImageButton(player1Drawable);
         hand2 = new ImageButton(player2Drawable);
+        writtenTutB = new ImageButton(writtenTutDraw);
+        writtenTutB.setTransform(true);
+        writtenTutB.setScale(2f);
+        back2B = new ImageButton(back2Draw);
+        back2B.setTransform(true);
+        back2B.setScale(2f);
 
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
@@ -73,6 +110,26 @@ public class PlayState extends State {
                 handleInput("settingsB");
             }
         });
+        quitB.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                handleInput("quitB");
+            }
+        });
+
+        backB.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                handleInput("backB");
+            }
+        });
+        tutB.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                handleInput("tutB");
+            }
+        });
+
 
         hand1.addListener(new ChangeListener() {
             @Override
@@ -87,13 +144,58 @@ public class PlayState extends State {
                 handleInput("player2");
             }
         });
+        back2B.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                handleInput("back2");
+            }
+        });
+        pinkWindow = new Window("Settings",
+                new Window.WindowStyle(new BitmapFont(), new Color(0, 0, 0, 0),
+                        windowDraw)) {
+            public void result(Object obj) {
+                System.out.println("result " + obj);
+            }
+        };
+        orangeWindow = new Window("Tutorial",
+                new Window.WindowStyle(new BitmapFont(), new Color(0, 0, 0, 0),
+                        tutWinDraw)) {
+            public void result(Object obj) {
+                System.out.println("result " + obj);
+            }
+        };
+
+        Table table = new Table();
+        // Add buttons to the table
+        table.add(tutB).padRight(Gdx.graphics.getWidth() / 2 + 30);
+        table.row().padTop(200); // Create a new row
+        table.add(backB).padRight(Gdx.graphics.getWidth() / 2 + 30);
+        table.row().padTop(185); // Create a new row
+        table.add(quitB).padRight(Gdx.graphics.getWidth() / 2 + 30);
+
+        pinkWindow.setResizable(true);
+        pinkWindow.add(table);
+        pinkWindow.pack();
+
+        Table table2 = new Table();
+
+        table2.add(writtenTutB).padRight(Gdx.graphics.getWidth() / 2 + 40)
+                .padTop(550);
+        table2.row().padTop(300);
+        table2.add(back2B).padRight(Gdx.graphics.getWidth() / 2 + 30);
+
+        //table2.add(back2B).padRight(Gdx.graphics.getWidth()/ 2 +30);
+        orangeWindow.add(table2);
+        orangeWindow.pack();
+
     }
 
     @Override
     protected final void handleInput(String name) {
         switch (name) {
             case "settingsB":
-                gsm.set(new MainMenuState(gsm));
+                //gsm.set(new MainMenuState(gsm));
+                stage.addActor(pinkWindow);
                 break;
             case "player1":
                 //Calling the movePlayer method from the Player class
@@ -105,6 +207,19 @@ public class PlayState extends State {
                 break;
             default:
                 // handle invalid input
+                break;
+            case "tutB":
+
+                stage.addActor(orangeWindow);
+                break;
+            case "quitB":
+                gsm.set(new MainMenuState(gsm));
+                break;
+            case "backB":
+                stage.addAction(Actions.removeActor(pinkWindow));
+                break;
+            case "back2":
+                stage.addAction(Actions.removeActor(orangeWindow));
                 break;
         }
     }
